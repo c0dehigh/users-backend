779c7cf3ad7b6035f9db3d9afe492220c6206838
Hamed zibandeh <high_dev@outlook.com>
2025-10-27 16:01:49 +0330

    add patch to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,26 +13,31 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
-  // optional {/:id}
+  // optional '{/:id}'
 
-  @Get('/:id')
+  @Get('{/:id}')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(limit);
-    console.log(page);
+    console.log(getUsersParamDto);
 
     return 'You send a get request to users endpoints';
   }
 
   @Post()
   public createUsers(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto);
     return 'You send a post request to users endpoints';
+  }
+
+  @Patch()
+  public patchUsers(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
