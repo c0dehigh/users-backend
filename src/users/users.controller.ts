a06be72ce1f8f074ec1160acb2f7810a37b04706
Hamed zibandeh <high_dev@outlook.com>
2025-11-05 14:37:12 +0330

    feat: implement UsersService and integrate with UsersController and UsersModule

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,9 +15,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
-
+import { UsersService } from './providers/users.service';
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   // optional '{/:id}'
 
   @Get('{/:id}')
