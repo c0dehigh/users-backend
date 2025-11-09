5735e3f6c51dba19a3ac78bb91ec9aaa4ecbc33e
Hamed zibandeh <high_dev@outlook.com>
2025-11-09 13:42:15 +0330

    add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { AuthService } from './providers/auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    /**
+     * Injecting Auth Service
+     */
+
+    private readonly authService: AuthService,
+  ) {}
+}
