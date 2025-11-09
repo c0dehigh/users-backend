4113a5429692429ef765b907e28be025ff0c5a0b
Hamed zibandeh <high_dev@outlook.com>
2025-11-09 09:34:36 +0330

    add some

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 
 @Controller('posts')
@@ -10,4 +10,9 @@ export class PostsController {
 
     private readonly postsService: PostsService,
   ) {}
+
+  @Get('{/:userId}')
+  public getPosts(@Param('userId') userId: string) {
+    this.postsService.findAll(userId);
+  }
 }
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  findAll(userId: string) {
+    console.log(`post service userid : ${userId}`);
+
+    return [
+      {
+        id: 1,
+        title: 'Post 1',
+        content: 'Content 1',
+      },
+      {
+        id: 2,
+        title: 'Post 2',
+        content: 'Content 2',
+      },
+    ];
+  }
+}
