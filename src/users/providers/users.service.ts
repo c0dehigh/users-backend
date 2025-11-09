1885f9b309e56961c505f677eef88026f1e68477
Hamed zibandeh <high_dev@outlook.com>
2025-11-09 13:43:18 +0330

    fix Circular Dependency

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
+import { AuthService } from 'src/auth/providers/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    /**
+     * injecting auth service
+     */
+
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
+
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
+    const isAuth = this.authService.isAuth();
+
     return [
       {
         firstName: 'John',
@@ -20,7 +32,7 @@ export class UsersService {
     ];
   }
 
-  public findOneById(id: number) {
+  public findOneById(id: string) {
     return {
       id: 1234,
       firstName: 'Alice',
