e33e47090e1cee6b441c6d5c0ee6d0a954623d2b
Hamed zibandeh <high_dev@outlook.com>
2025-11-11 17:42:21 +0330

    add create post method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Controller('posts')
 @ApiTags('posts')
@@ -12,6 +13,9 @@ export class PostsController {
 
     private readonly postsService: PostsService,
   ) {}
+
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {}
 
   @Get('{/:userId}')
   public getPosts(@Param('userId') userId: string) {
