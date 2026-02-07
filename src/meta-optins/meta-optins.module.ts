81a255be9e7c79dfef311c990effdd28f28a7ba6
Hamed zibandeh <high_dev@outlook.com>
2026-02-07 16:48:03 +0330

    fix: add TypeOrmModule import and configure MetaOption entity in MetaOptinsModule

--- src/meta-optins/meta-optins.module.ts
+++ src/meta-optins/meta-optins.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MetaOptinsController } from './meta-optins.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MetaOption } from './meta-option.entity';
 
 @Module({
-  controllers: [MetaOptinsController]
+  controllers: [MetaOptinsController],
+  imports: [TypeOrmModule.forFeature([MetaOption])],
 })
 export class MetaOptinsModule {}
