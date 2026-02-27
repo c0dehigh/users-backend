b1e850c4300fd13821021324039f3d0f0102f38e
Hamed zibandeh <hamedzibandeh70@gmail.com>
2026-02-27 14:11:15 -0500

    start paganation

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -13,6 +13,7 @@ import { PostsService } from './providers/posts.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
 import { PatchPostDto } from './dtos/patch-post-dto';
+import { GetPostsDto } from './dtos/get-posts.dto';
 
 @Controller('posts')
 @ApiTags('posts')
@@ -38,7 +39,12 @@ export class PostsController {
   }
 
   @Get('{/:userId}')
-  public getPosts(@Param('userId') userId: string) {
+  public getPosts(
+    @Param('userId') userId: string,
+    @Query() postQuery: GetPostsDto,
+  ) {
+    console.log(postQuery);
+
     return this.postsService.findAll(userId);
   }
   @ApiResponse({
