1e12b1eea1167549683558f1d01ede10cf514b70
Hamed zibandeh <high_dev@outlook.com>
2026-04-19 11:58:21 +0330

    add sign in method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
+import { SignInDto } from './dtos/signin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +11,9 @@ export class AuthController {
 
     private readonly authService: AuthService,
   ) {}
+  @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
+  public async signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
 }
