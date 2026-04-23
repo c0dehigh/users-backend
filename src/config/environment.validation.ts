363c30277f0212052ea2839e62a5c5281b07b9a5
Hamed zibandeh <high_dev@outlook.com>
2026-04-23 21:28:32 +0330

    feat: add JWT configuration parameters to environment validation

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -11,4 +11,8 @@ export default Joi.object({
   DATABASE_PASSWORD: Joi.string().required(),
   DATABASE_NAME: Joi.string().required(),
   PROFILE_API_KEY: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.string().required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
 });
