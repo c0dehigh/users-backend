280268a1d8f6b10b42d90f3fe1bb530a83504778
Hamed zibandeh <high_dev@outlook.com>
2026-04-24 14:48:09 +0330

    feat: add @Auth decorator to set metadata for authentication types

---
+++ src/auth/decorator/auth.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { AuthType } from '../enums/auth-type.enum';
+import { AUTH_TYPE_KEY } from '../constants/auth.constants';
+
+export const Auth = (...AuthTypes: AuthType[]) =>
+  SetMetadata(AUTH_TYPE_KEY, AuthTypes);
